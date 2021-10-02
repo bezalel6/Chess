c880013229f8a8d3dc3c14dfc3f5325e25db3472
bezalel6 <bezalel3250@gmail.com>
2021-10-03 00:43:29 +0300

    knb

---
+++ src/ver26_save_minimax_levels/view_classes/BoardPanel.java
@@ -0,0 +1,10 @@
+package ver26_save_minimax_levels.view_classes;
+
+import javax.swing.*;
+
+public class BoardPanel extends JPanel {
+
+    public BoardPanel() {
+
+    }
+}
---
+++ src/ver26_save_minimax_levels/view_classes/MyJButton.java
@@ -0,0 +1,27 @@
+package ver26_save_minimax_levels.view_classes;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.function.Function;
+
+public class MyJButton extends JButton {
+    public MyJButton(String text) {
+        super(text);
+        setFocusable(false);
+    }
+
+    public MyJButton() {
+        this("");
+    }
+
+    public MyJButton(String text, Font font, Function<Void, Void> onClick, JPanel addTo) {
+        this(text, font, onClick);
+        addTo.add(this);
+    }
+
+    public MyJButton(String text, Font font, Function<Void, Void> onClick) {
+        this(text);
+        setFont(font);
+        addActionListener(e -> onClick.apply(null));
+    }
+}
